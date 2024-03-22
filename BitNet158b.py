f2ad122a34c5a5d61e69949d7e249c96532785dc
suito555 <162005396+suito555@users.noreply.github.com>
2024-03-22 23:54:15 +0900

    Update BitNet158b.py

--- BitNet158b.py
+++ BitNet158b.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-class RMSNorm(nn.Module): #quoted LammaRMSNorm
+class RMSNorm(nn.Module): #quoted by LammaRMSNorm
     def __init__(self, hidden_size, eps=1e-6):
         super().__init__()
         self.weight = nn.Parameter(torch.ones(hidden_size))
@@ -15,28 +15,25 @@ class RMSNorm(nn.Module): #quoted LammaRMSNorm
         hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
         return self.weight * hidden_states.to(input_dtype)
 
-
 class BitLinear158b(nn.Linear):
     def __init__(self, in_features, out_features, bias=False):
         super(BitLinear158b, self).__init__(in_features, out_features, bias)
         self.rmsnorm = RMSNorm(in_features)
         
     def activation_norm_quant(self, x):
-        x = self.rmsnorm(x)
         scale = 127.0 / x.abs().max(dim=-1,keepdim=True).values.clamp(min=1e-5) #gamma
         y = (x * scale).round().clamp(-128,127)
-        return y,scale
+        return y
     
     def weight_quant(self,w):
         scale = 1.0 / w.abs().mean().clamp(min=1e-5) #beta
         u = (w * scale).round().clamp(-1,1)
-        
-        return w + (u - w).detach(),scale #STE
+        return u
 
-    def forward(self, input_ts): 
-        x_quant,x_scale = self.activation_norm_quant(input_ts)
-        w_quant,w_scale = self.weight_quant(self.weight)
-
-        output = F.linear(x_quant,w_quant,self.bias) / w_scale / x_scale
+    def forward(self, x):
+        w = self.weight
+        x_norm = self.rmsnorm(x)
+        x_quant = x_norm + (self.activation_norm_quant(x_norm) - x_norm).detach()
+        w_quant = w + (self.weight_quant(w) - w).detach()
+        output = F.linear(x_quant,w_quant,self.bias)
         return output
-
